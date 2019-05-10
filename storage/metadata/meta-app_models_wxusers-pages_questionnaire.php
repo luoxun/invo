5f97edbd611be30f206598d95ba2cae7dc95a4e3
luoxun <luoxun007@gmail.com>
2019-05-10 16:15:11 +0800

    up tmp

---
+++ storage/metadata/meta-app_models_wxusers-pages_questionnaire.php
@@ -0,0 +1,83 @@
+<?php return array (
+  0 => 
+  array (
+    0 => 'id',
+    1 => 'is_active',
+    2 => 'title',
+    3 => 'desc',
+    4 => 'created_at',
+    5 => 'updated_at',
+    6 => 'deleted_at',
+    7 => 'embody',
+    8 => 'op',
+  ),
+  1 => 
+  array (
+    0 => 'id',
+  ),
+  2 => 
+  array (
+    0 => 'is_active',
+    1 => 'title',
+    2 => 'desc',
+    3 => 'created_at',
+    4 => 'updated_at',
+    5 => 'deleted_at',
+    6 => 'embody',
+    7 => 'op',
+  ),
+  3 => 
+  array (
+    0 => 'id',
+  ),
+  4 => 
+  array (
+    'id' => 0,
+    'is_active' => 0,
+    'title' => 2,
+    'desc' => 2,
+    'created_at' => 4,
+    'updated_at' => 4,
+    'deleted_at' => 4,
+    'embody' => 2,
+    'op' => 2,
+  ),
+  5 => 
+  array (
+    'id' => true,
+    'is_active' => true,
+  ),
+  8 => 'id',
+  9 => 
+  array (
+    'id' => 1,
+    'is_active' => 1,
+    'title' => 2,
+    'desc' => 2,
+    'created_at' => 2,
+    'updated_at' => 2,
+    'deleted_at' => 2,
+    'embody' => 2,
+    'op' => 2,
+  ),
+  10 => 
+  array (
+  ),
+  11 => 
+  array (
+  ),
+  12 => 
+  array (
+    'is_active' => NULL,
+    'title' => NULL,
+    'desc' => NULL,
+    'created_at' => NULL,
+    'updated_at' => NULL,
+    'deleted_at' => NULL,
+    'embody' => NULL,
+    'op' => NULL,
+  ),
+  13 => 
+  array (
+  ),
+); 
